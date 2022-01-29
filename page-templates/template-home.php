8914a46be656b73a96aaff4693eb375e18083ff6
Vlad9999 <vladchervjakov@gmail.com>
2022-01-30 01:17:09 +0200

    finished home call to action

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -19,19 +19,19 @@ get_header();
 <div class="home-call-to-action">
     <div class="container">
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <a href="#" class="call-to-action">
                     <div class="image"></div>
                     <div class="title">Dining Menu</div>
                 </a>
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <a href="#" class="call-to-action second">
                     <div class="image"></div>
                     <div class="title">Lunch Menu</div>
                 </a>
             </div>
-            <div class="col-lg-4">
+            <div class="col-md-4">
                 <a href="#" class="call-to-action third">
                     <div class="image"></div>
                     <div class="title">Functions</div>
