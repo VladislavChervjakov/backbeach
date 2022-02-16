5aeaeeac2e8e62701b289fbe94846812ce22341c
Vlad9999 <vladchervjakov@gmail.com>
2022-02-16 22:20:14 +0200

    started footer

--- footer.php
+++ footer.php
@@ -1,2 +1,23 @@
+<div class="wrapper" id="wrapper-footer">
+    <div class="bg-image"></div>
+
+    <div class="title-holder">
+        <div class="container">
+            <h2>
+                <span>Contact</span>
+                Send us a message bellow
+            </h2>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1"></div>
+            <div class="col-lg-11">
+                <?php echo do_shortcode( '[contact-form-7 id="22" title="Footer Form"]' ); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php
 wp_footer();
